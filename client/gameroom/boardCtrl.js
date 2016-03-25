2b28d2f11254890fefcaa68ef76533a514832bb1
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-24 18:39:34 -0700

    [feature] basic board and minor css styling

--- client/gameroom/boardCtrl.js
+++ client/gameroom/boardCtrl.js
@@ -3,12 +3,39 @@ angular
     .controller('Board', [ '$scope', 'BoardLogic', function ($scope, BoardLogic) {
 
       $scope.box = ['nope1', 'nope2'];
-
       $scope.test = function () {
         console.log('math');
         console.log(this.box);
         this.box.push("nope"+($scope.box.length+1));
       }
 
-    }])
+      $scope.board = BoardLogic.board;
+      $scope.log = function(row, columb, piece) {
+        console.log('first step : ', piece, '--', '['+row+'.'+columb+']');
+        if(BoardLogic.currentPiece.piece === null) {
+          BoardLogic.currentPiece = {
+            piece: piece,
+            row: row,
+            columb: columb
+          };
+        }
+        else if(BoardLogic.currentPiece.row === row &&
+           BoardLogic.currentPiece.columb === columb &&
+           BoardLogic.currentPiece.piece === piece) {
+            console.log('same piece, reset');
+            BoardLogic.currentPiece = BoardLogic.defaultPiece;
+        }
+        //  -- FIX MY LOGIC --
+        // else if (-1 <= BoardLogic.currentPiece.row - row >= 1 &&
+        //          -1 <= BoardLogic.currentPiece.columb - columb <= 1 ){
+        //   console.log("we can move");
+        // }
+        else {
+          console.log('its a diffrent square your clicking on');
+        }
 
+        console.log(BoardLogic.currentPiece);
+      }
+
+
+    }])
--- client/gameroom/boardFactory.js
+++ client/gameroom/boardFactory.js
@@ -1,22 +1,43 @@
 angular
   .module('rts.board.logic', [])
-    .factory('BoardLogic', ['$scope', '$state', function ($scope, $state) {
+    .factory('BoardLogic', function () {
 
+      var defaultPiece = {
+        piece: null,
+        row: null,
+        columb: null
+      }
+      var currentPiece = {
+        piece: null,
+        row: null,
+        columb: null
+      }
       var board = [];
       var makeBoard = function (size) {
+        console.log('lets make a board');
         for(var i = 0; i < size; i++) {
           var row = [];
           for(var j = 0; j < size; j++) {
-            row.push( {
-              peice: null,
-              canUse: false
-            })
+            var square = {
+              piece: null,
+              canUse: false,
+              highlight: false
+            };
+            if(Math.random()*10 < 1 ) {
+              square.piece = 'pawn';
+            }
+            row.push(square);
           }
+          board.push(row);
         }
       };
 
       makeBoard(8);
       console.log('The board : ', board);
-      return board;
+      return {
+        board: board,
+        currentPiece: currentPiece,
+        defaultPiece: defaultPiece
+      };
 
-    }])
+    })
